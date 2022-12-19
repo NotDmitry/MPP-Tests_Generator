6196f6a4ad6b3d48846faa764b3e8469b5bf2ff9
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-20 02:12:34 +0300

    Initial additional

---
+++ Tests_Generator.Core/TargetContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace Tests_Generator.Core;
+
+public class TargetContainer
+{
+    public string Path { get; set; }
+    public string Code { get; set; }
+
+    public TargetContainer(string path, string code)
+    {
+        Path = path;
+        Code = code;
+    }
+}
